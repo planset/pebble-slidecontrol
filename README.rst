2d654ad682e2166e9b3263ddcc6cf7b1b0c268ac
Daisuke Igarashi <daisuke@dkpyn.com>
2014-10-11 04:14:13 +0900

    pythonをやめてgoにした。

--- README.rst
+++ README.rst
@@ -9,9 +9,6 @@ pebbleからkeynoteのページを進めたり戻したりするためのプロ�
 
 pebbleの時計のインストール方法
 ==============================
-pebble/config.htmlをどこかアクセスできるところにおいて、pebble/src/js/pebble-js-app.jsのconfigUrlを変更して下さい。
-（そのままでもいいですが、http://dkpyn.com/sandbox/pebble/config.htmlが急になるなるかもしれませんので、そこはご了承下さい）
-
 ::
 
     cd pebble
@@ -19,16 +16,36 @@ pebble/config.htmlをどこかアクセスできるところにおいて、pebbl
     pebble install
 
 
+インストールしたらスマートフォンのPebbleアプリでインストールしたアプリのSettingsを開き、MacのIP:18888 を入力して保存します。
+
+MacのIPが192.168.0.10の場合には、192.168.0.10:18888と入力します。
+
+注意
+----
+設定画面に使っているpebble/config.htmlは http://dkpyn.com/sandbox/pebble/config.html においてありますが、私が突如消す可能性もあります。そのときはごめんなさい。
+
+
 server側
 ========
-kyenoteを起動している端末でpythonプログラムを動かします。
-
-::
+kyenoteを起動している端末でhttp_serverプログラムを起動します。::
 
     cd server
-    virtualenv .env
-    source .env/bin/activate
-    pip install -r requirements.txt
-    python ws_server.py
+    ./http_server
+
+またはポートを指定して起動します。::
+
+    ./http_server 9999
+
+ポートはデフォルトでは18888です。こちらの起動ポートを変えた場合には、Pebble側のSettingsのポートも変更して下さい。
+
+起動すると以下の様な表示になり、pebble側が正しく動いていればボタンを押すとnextやbackが表示されます。::
+    
+    Starting server ... 0.0.0.0:18888
+    next
+    back
+
+
+この状態でKeynoteを開き、プレゼンを開始してからPebbleのボタンを押すと、スライドを操作することができます。
+
 
 
--- pebble/src/js/pebble-js-app.js
+++ pebble/src/js/pebble-js-app.js
@@ -1,39 +1,42 @@
 var ws;
-var configUrl = 'http://dkpyn.com/sandbox/pebble/config.html';
+var configUrl = "http://dkpyn.com/sandbox/pebble/config.html";
 
 Pebble.addEventListener("ready",
     function(e) {
-        console.log('ready');
+        console.log("ready");
     }
 );
 
-Pebble.addEventListener("appmessage",
-    function(e) {
-        console.log(e.payload.control);
-        if (ws == null) {
-            var host = localStorage.getItem('host');
-            ws = new WebSocket('ws://' + host + '/ws');
-            ws.onopen = function() {
-                ws.send(e.payload.control.toString());
-                console.log('send');
-            }
-        } else {
-            ws.send(e.payload.control.toString());
-            console.log('send');
-        }
+function controlIdToUrl(id) {
+    if (id == 1) {
+        return "next";
+    } else {
+        return "back";
     }
-)
+}
+
+Pebble.addEventListener("appmessage", function(e) {
+    console.log(e.payload.control);
+
+    var host = localStorage.getItem("host");
+    var url = "http://" + host + "/" + controlIdToUrl(e.payload.control);
+
+    var req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.send();
+
+});
 
 Pebble.addEventListener("showConfiguration", function() {
-  console.log("showing configuration");
-  var options = {host: localStorage.getItem('host')};
-  var encodedOptions = encodeURIComponent(JSON.stringify(options));
-  Pebble.openURL(configUrl + '#' + encodedOptions);
+    console.log("showing configuration");
+    var options = {host: localStorage.getItem('host')};
+    var encodedOptions = encodeURIComponent(JSON.stringify(options));
+    Pebble.openURL(configUrl + '#' + encodedOptions);
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
-  console.log("configuration closed");
-  options = JSON.parse(decodeURIComponent(e.response));
-  localStorage.setItem('host', options.host);
-  console.log("Options = " + JSON.stringify(options));
+    console.log("configuration closed");
+    options = JSON.parse(decodeURIComponent(e.response));
+    localStorage.setItem('host', options.host);
+    console.log("Options = " + JSON.stringify(options));
 });;
